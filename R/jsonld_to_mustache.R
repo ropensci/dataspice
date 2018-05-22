85f37da677c9eb17976b225aba495e9af5614883
Bryce Mecum <petridish@gmail.com>
2018-05-22 15:10:14 -0700

    Finish implemenation of bounding box display

--- R/jsonld_to_mustache.R
+++ R/jsonld_to_mustache.R
@@ -25,14 +25,60 @@ jsonld_to_mustache <- function(path) {
   json$beginDate <- tc[1]
   json$endDate <- tc[2]
 
-  json <- c(json, parse_geographic_coverage)
+  json <- c(json, parse_spatialCoverage(json$spatialCoverage))
 
   json
 }
 
-parse_geographic_coverage <- function(geographicCoverage) {
-  json <- list()
-  json$mapcenterstring <- "[50, -150]"
-  json$mappolystring <- "[[78, -131],[78, -171],[47, -171],[47, -131]]"
+#' Parse spatialCoverge section for use in a Leaflet map
+#'
+#' @param spatialCoverage (list) spatialCoverage section of the JSONLD
+#'
+#' @return (list) Template-specific variables for Leaflet
+parse_spatialCoverage <- function(spatialCoverage) {
+  json <- list(showmap = "geo" %in% names(spatialCoverage) && "type" %in% names(spatialCoverage$geo),
+               isgeoshape = ifelse(spatialCoverage$geo$type == "GeoShape", TRUE, FALSE),
+               isgeopoints = ifelse(spatialCoverage$geo$type == "GeoPoints", TRUE, FALSE))
+
+  if (json$isgeoshape) {
+    json <- c(json, parse_GeoShape_box(spatialCoverage$geo$box))
+  } else if (json$isgeopoints) {
+    json <- c(json, parse_GeoShape_box(spatialCoverage$geo$points))
+
+  } else {
+    stop("Specified spatialCoverage not supported.", call. = FALSE)
+  }
+
   json
+}
+
+#' Parse spatialCoverge$geo$box section for use in a Leaflet map
+#'
+#' @param box (list) spatialCoverage$geo$box section of the JSONLD
+#'
+#' @return (list) Template-specific variables for Leaflet
+parse_GeoShape_box <- function(box) {
+  tokens <- stringr::str_split(box, " ")
+
+  if (!length(tokens) == 1) {
+    stop("Failed to parse box in spatialCoverage$geo$box of '", box, "'.", call. = FALSE)
+  }
+
+  tokens <- vapply(tokens[[1]], as.numeric, 0.0, USE.NAMES = FALSE)
+
+  list(mapcenterstring = paste0("[",
+                                (tokens[1] + tokens[1] + tokens[3] + tokens[3]) / 4,
+                                ", ",
+                                (tokens[2] + tokens[2] + tokens[4] + tokens[4]) / 4,
+                                "]"),
+       mappolystring = paste0("[",
+         "[ ", tokens[1], ", ", tokens[2], "], ",
+         "[ ", tokens[3], ", ", tokens[2], "], ",
+         "[ ", tokens[3], ", ", tokens[4], "], ",
+         "[ ", tokens[1], ", ", tokens[4], "]",
+       "]"))
+}
+
+parse_GeoShape_points <- function(points) {
+  stop("Not implemented.", call. = FALSE)
 }
