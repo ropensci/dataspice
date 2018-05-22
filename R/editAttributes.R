a89acf05d6c4080dd719aa70ee0d0ca5926b411d
annakrystalli <annakrystalli@googlemail.com>
2018-05-22 16:46:21 -0700

    correct edit* functions

--- R/editAttributes.R
+++ R/editAttributes.R
@@ -3,6 +3,7 @@
 #' @param DF the imported attributes.csv dataframe
 
 #' @export
+#' @import shiny
 #'
 #' @examples
 #' \dontrun{
@@ -10,64 +11,64 @@
 #'
 #'}
 
-editAttributes <- function(DF, 
-                      outdir=getwd(), 
+editAttributes <- function(DF,
+                      outdir=getwd(),
                       outfilename="attributes"){
   ui <- shinyUI(fluidPage(
-    
+
     titlePanel("Populate the Attributes Metadata Table"),
     sidebarLayout(
       sidebarPanel(
         helpText("Shiny app to read in the dataspice metadata templates and populate with usersupplied data"),
-        
+
         h6('fileName = the name of the input data file(s). Do Not Change.'),
         h6("variableName = the name of the measured variable. Do Not Change."),
         h6('Description = a written description of what that measured variable is'),
         h6("unitText = the units the variable was measured in"),
-      
-        br(), 
-        
+
+        br(),
+
         wellPanel(
-          h3("Save table"), 
-          div(class='row', 
-              div(class="col-sm-6", 
+          h3("Save table"),
+          div(class='row',
+              div(class="col-sm-6",
                   actionButton("save", "Save"))
           )
         )
-        
+
       ),
-      
+
       mainPanel(
         wellPanel(
           uiOutput("message", inline=TRUE)
         ),
         rHandsontableOutput("hot"),
          br()
-        
+
       )
     )
   ))
-  
+
   server <- shinyServer(function(input, output) {
-    
+
     values <- reactiveValues()
 
     output$hot <- renderRHandsontable({
         DF$description <- as.character(DF$description)
         DF$unitText <- as.character(DF$unitText)
-        rhandsontable(DF, 
-                      useTypes = TRUE, 
+        rhandsontable(DF,
+                      useTypes = TRUE,
                       stretchH = "all")
     })
-    
-    ## Save 
+
+    ## Save
     observeEvent(input$save, {
       finalDF <- hot_to_r(input$hot)
-      write.csv(finalDF, file=file.path(outdir, 
+      write.csv(finalDF, file=file.path(outdir,
                         sprintf("%s.csv", outfilename)),
                 row.names = FALSE)
     })
-    
+
     ## Message
     output$message <- renderUI({
       if(input$save==0){
@@ -81,10 +82,10 @@ editAttributes <- function(DF,
                               fun, outfile)))
       }
     })
-    
+
   })
-  
-  ## run app 
+
+  ## run app
   runApp(list(ui=ui, server=server))
   return(invisible())
 }
--- R/editCreators.R
+++ R/editCreators.R
@@ -1,7 +1,8 @@
 #' Shiny App for editing the metadata creator table
 #'
 #' @param DF the imported creator.csv dataframe
-#' @param numCreators the number of creators that need to be included, defauls to 10 
+#' @param numCreators the number of creators that need to be included, defauls to 10
+#' @import shiny
 #' @export
 #'
 #' @examples
@@ -10,78 +11,78 @@
 #'
 #'}
 
-editCreators <- function(DF, 
-                           outdir=getwd(), 
+editCreators <- function(DF,
+                           outdir=getwd(),
                            outfilename="creator",
                          numCreators=10){
   ui <- shinyUI(fluidPage(
-    
+
     titlePanel("Populate the Creators Metadata Table"),
     sidebarLayout(
       sidebarPanel(
         helpText("Shiny app to read in the dataspice metadata templates and populate with usersupplied data"),
-        
+
         h6('fileName = the name of the input data file(s). Do Not Change.'),
         h6("variableName = the name of the measured variable. Do Not Change."),
         h6('Description = a written description of what that measured variable is'),
         h6("unitText = the units the variable was measured in"),
-        
-        br(), 
-        
+
+        br(),
+
         wellPanel(
-          h3("Save table"), 
-          div(class='row', 
-              div(class="col-sm-6", 
+          h3("Save table"),
+          div(class='row',
+              div(class="col-sm-6",
                   actionButton("save", "Save"))
           )
         )
-        
+
       ),
-      
+
       mainPanel(
         wellPanel(
           uiOutput("message", inline=TRUE)
         ),
         rHandsontableOutput("hot"),
         br()
-        
+
       )
     )
   ))
-  
+
   server <- shinyServer(function(input, output) {
-    
+
     values <- reactiveValues()
-    
-   
-    
+
+
+
     output$hot <- renderRHandsontable({
 
       rows_to_add <- as.data.frame(matrix(nrow=numCreators,
                                           ncol=ncol(DF)))
-      
+
       colnames(rows_to_add) <- colnames(DF)
       DF <- rows_to_add
-      DF$ï..name <- as.character(DF$ï..name)
+      DF$name <- as.character(DF$ï..name)
       DF$type <- as.character(DF$type)
       DF$id <- as.character(DF$id)
       DF$givenName <- as.character(DF$givenName)
       DF$familyName <- as.character(DF$familyName)
       DF$affilitation <- as.character(DF$affilitation)
       DF$email <- as.character(DF$email)
-      rhandsontable(DF, 
-                    useTypes = TRUE, 
+      rhandsontable(DF,
+                    useTypes = TRUE,
                     stretchH = "all")
     })
-    
-    ## Save 
+
+    ## Save
     observeEvent(input$save, {
       finalDF <- hot_to_r(input$hot)
-      write.csv(finalDF, file=file.path(outdir, 
+      write.csv(finalDF, file=file.path(outdir,
                                         sprintf("%s.csv", outfilename)),
                 row.names = FALSE)
     })
-    
+
     ## Message
     output$message <- renderUI({
       if(input$save==0){
@@ -95,10 +96,10 @@ editCreators <- function(DF,
                               fun, outfile)))
       }
     })
-    
+
   })
-  
-  ## run app 
+
+  ## run app
   runApp(list(ui=ui, server=server))
   return(invisible())
 }
