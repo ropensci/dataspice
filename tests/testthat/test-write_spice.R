f01aea789c2e1fb9394a7b7806ffc52d7f3e5af3
Bryce Mecum <petridish@gmail.com>
2018-05-24 20:13:47 -0800

    Add a test for write_spice
    Also fixed a bug in serialization of access.csv in metadata-tables

---
+++ tests/testthat/test-write_spice.R
@@ -0,0 +1,23 @@
+context("write_spice")
+
+test_that("write spice creates a dataspice.json in the right place", {
+  tmp <- tempdir()
+  on.exit(unlink(tmp, recursive = TRUE))
+
+  unlink(file.path(tmp, "metadata"), recursive = TRUE)
+  dir.create(file.path(tmp, "metadata"))
+
+  files_to_copy <- dir(system.file("metadata-tables", package = "dataspice"),
+                       pattern = ".csv",
+                       full.names = TRUE)
+
+  lapply(files_to_copy, function(path) {
+    file.copy(path, file.path(tmp, "metadata", basename(path)), overwrite = TRUE)
+  })
+
+  write_spice(file.path(tmp, "metadata"))
+  dataspice_path <- file.path(tmp, "metadata", "dataspice.json")
+
+  expect_true(file.exists(dataspice_path))
+  expect_true(file.size(dataspice_path) > 0)
+})
