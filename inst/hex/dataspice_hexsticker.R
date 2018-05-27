cc93e5ce3e08eb2f8d0e95436eba3e40ff1dfc85
aurielfournier <aurielfournier@gmail.com>
2018-05-27 13:24:50 -0500

    first stab at hexsticker

---
+++ inst/hex/dataspice_hexsticker.R
@@ -0,0 +1,20 @@
+#hexsticker
+library(hexSticker)
+
+
+
+p.1 <- sticker(a,
+               package="dataspice", 
+               s_x = 1, # horizontal position of subplot
+               s_y = 1.1, # vertical position of subplot
+               s_width = 1.5, # width of subplot
+               s_height = 1.5, # height of subplot
+               p_x = 1, # horizontal position of font
+               p_y = .53, # vertical position of font
+               p_size = 10, # font size
+               p_color = "#000000", # font colour
+               h_size = 7, # hexagon border size
+               h_fill = "white", # hexagon fill colour
+               h_color = "red") # hexagon border colour
+
+
