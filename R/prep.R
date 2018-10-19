6a43c77f3df888e3cdf04b5b74526905fb545af9
Bryce Mecum <petridish@gmail.com>
2018-10-18 21:33:59 -0800

    Use non-shorthand for T (TRUE)

--- R/prep.R
+++ R/prep.R
@@ -21,7 +21,7 @@
 #' # extract attributes from all `csv`, `tsv`, `rds` files in the data folder (non recursive)
 #' prep_attributes()
 #' # recursive
-#' prep_attributes(recursive = T)
+#' prep_attributes(recursive = TRUE)
 #' # extract attributes from a single file using file path
 #' data_path <- system.file("example-dataset","BroodTables.csv", package = "dataspice")
 #' prep_attributes(data_path)
